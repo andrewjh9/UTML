0515e2c916c64efca7c9c0582de65d49ed47f33c
MartenMoti <s2190532@utwente.nl>
2021-02-02 13:59:13 +0100

    Add todo and comment about the disabling of security

--- src/main/java/nl/utwente/utml/WebSecurityConfig.java
+++ src/main/java/nl/utwente/utml/WebSecurityConfig.java
@@ -11,6 +11,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // TODO: Replace this with something that actually handles security instead of disabling it.
+        // This disables security.
+        // This was done because we got 401 Unauthorized errors during debugging on the very first day.
+        // This will later be removed or edited to actually provide security
         http.csrf().disable();
     }
 
