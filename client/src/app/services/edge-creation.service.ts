33f78def7c403ce10a8081c1c67c6a4e503eaec8
= <s2190532@utwente.nl>
2021-03-12 10:20:48 +0100

    Fix bug where clicking on attachment point would create an edge with same start and finish

--- client/src/app/services/edge-creation.service.ts
+++ client/src/app/services/edge-creation.service.ts
@@ -41,6 +41,10 @@ export class EdgeCreationService implements Deactivatable {
   }
 
   public setEnd(endNode: Node, endAttachment: AttachmentDirection) {
+    if (endNode === this.startNode && endAttachment === this.startAttachment) {
+      return this.deactivate();
+    }
+
     if (!this.isActive()) {
       throw new Error("Trying to set end whilst it is inactive!");
     }
