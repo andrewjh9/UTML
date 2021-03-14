ab278ad8501ced7081aea6f336c96de4b99fa39f
Platon Frolov <p.m.frolov@student.utwente.nl>
2021-03-14 15:18:22 +0100

    Editing works, only going out of edit mode is not possible yet, dont know how to fix it

--- client/src/app/services/edit.service.ts
+++ client/src/app/services/edit.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable, Renderer2, RendererFactory2} from '@angular/core';
 import {Node} from "../../model/node/node";
+import {Edge} from "../../model/edge";
+import {KeyboardEventCallerService} from "./keyboard-event-caller.service";
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +9,33 @@ import {Node} from "../../model/node/node";
 export class EditService {
   private isInEditMode?: boolean;
   private node?: Node;
-  constructor() { }
+  private renderer: Renderer2;
+  private activeIndex?: number;
+  constructor(private keyboardEventCallerService: KeyboardEventCallerService,
+              rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    this.renderer.listen('window', 'keydown', (event: KeyboardEvent) => {
+      if (this.isActive()) {
+        this.update(this.activeIndex!, event.key)
+      }
+    });
+  }
 
   public activate(node: Node) {
     this.node = node;
     this.isInEditMode = true;
   }
 
-  public getText(index: number) {
-
+  public setActive(index: number) {
+    console.log(index);
+    console.log(this.activeIndex)
+    if (this.activeIndex != index){
+      this.removeBar(this.activeIndex);
+      this.activeIndex = index;
+      let currentText: string[] = this.node!.getTextLines();
+      currentText[index] += "|";
+      this.setTextToNode(currentText);
+    }
   }
 
   public isActive(): boolean {
@@ -27,26 +47,61 @@ export class EditService {
       let currentText: string[] = this.node!.getTextLines();
       let newFieldDefaultText = "New field";
       currentText.push(newFieldDefaultText);
-      let textContent = "";
-      for (let i = 0; i < currentText.length - 1; i++) {
-        textContent += currentText[i] + "\\n"
-      }
-      textContent += currentText[currentText.length - 1];
-      this.node!.text = textContent;
+     this.setTextToNode(currentText)
     }
-
   }
 
   public update(index: number, text: string): void {
     if (this.isActive()) {
       let currentText: string[] = this.node!.getTextLines();
-      currentText[index] = text;
-      let textContent = "";
-      for (let i = 0; i < currentText.length - 1; i++) {
-        textContent += currentText[i] + "\\n"
+      if (text.length == 1 && text != "|") {
+        currentText[index] = currentText[index].split("|")[0] + text + "|"
+          +currentText[index].split("|")[1];
+      } else if (text == "Backspace") {
+        currentText[index] = currentText[index].split("|")[0].slice(0,-1) +
+          "|" + currentText[index].split("|")[1];
+      } else if (text == "ArrowLeft") {
+        let arr: string[] = currentText[index].split("|");
+        currentText[index] = this.swapFirstAndLastAndAppend(arr[0], "|" + arr[1])
+      } else if (text == "ArrowRight") {
+        let arr: string[] = currentText[index].split("|");
+        currentText[index] = this.swapFirstAndLastAndAppend(arr[0] + "|", arr[1])
       }
-      textContent += currentText[currentText.length - 1];
-      this.node!.text = textContent;
+      this.setTextToNode(currentText);
+      if (text == "ArrowUp") {
+        if (this.activeIndex! > 0) {
+          this.setActive(this.activeIndex! - 1);
+        }
+      } else if (text == "ArrowDown") {
+        if (this.activeIndex! < this.node!.getTextLines().length - 1) {
+          this.setActive(this.activeIndex! + 1);
+        }
+      }
+    }
+  }
+
+  private swapFirstAndLastAndAppend(string1: string, string2: string): string {
+    let lastOfFirst = string1[string1.length - 1];
+    let firstOfLast = string2[0];
+    string1 = string1.slice(0,-1) + firstOfLast;
+    string2 = lastOfFirst + string2.substring(1);
+    return string1 + string2;
+  }
+
+  private setTextToNode(currentText: string[]): void {
+    let textContent = "";
+    for (let i = 0; i < currentText.length - 1; i++) {
+      textContent += currentText[i] + "\\n"
+    }
+    textContent += currentText[currentText.length - 1];
+    this.node!.text = textContent;
+  }
+
+  private removeBar(index: number | undefined): void {
+    if (index !== undefined) {
+      let currentText = this.node!.getTextLines();
+      currentText[index] = currentText[index].slice(0, -1);
+      this.setTextToNode(currentText);
     }
   }
 
