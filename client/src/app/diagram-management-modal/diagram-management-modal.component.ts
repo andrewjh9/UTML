762a042c6ef9b67b1b936eb20f9b18f9c0e5e265
= <s2190532@utwente.nl>
2021-03-16 16:05:13 +0100

    Implement basic diagram management modal

--- client/src/app/diagram-management-modal/diagram-management-modal.component.ts
+++ client/src/app/diagram-management-modal/diagram-management-modal.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Node} from "../../model/node/node";
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {SelectionService} from "../services/selection.service";
 import {DeletionService} from "../services/deletion.service";
 import {CachingService} from "../services/caching/caching.service";
@@ -16,27 +16,37 @@ import {DiagramContainerService} from "../services/diagram-container.service";
   templateUrl: './diagram-management-modal.component.html',
   styleUrls: ['./diagram-management-modal.component.scss']
 })
-export class DiagramManagementModalComponent {
+export class DiagramManagementModalComponent implements OnInit{
+  @ViewChild('deleteModal') deleteModal!: ElementRef;
+  @ViewChild('editModal') editModal!: ElementRef;
+
   dbEntries: Array<DatabaseDiagramEntry>;
   selectedIndex = -1;
+
+  ngOnInit() {
+    // Set the dbEntire here. This method is called anytime the modal is opnened.
+  }
 
   get selectedDiagram(): Diagram | undefined {
     return this.selectedIndex === -1 ? undefined : deserialiseDiagram(this.dbEntries[this.selectedIndex].serialisedDiagram);
   }
 
   constructor(public modal: NgbActiveModal,
+              private modalService: NgbModal,
               private diagramContainer: DiagramContainerService) {
     this.dbEntries = [
       {
         id: 1,
         title: 'My Own Class Diagram',
         lastModified: new Date(2021, 2, 15),
+        visible: true,
         serialisedDiagram: cd.serialise()
       },
       {
         id: 2,
         title: 'My Own FSM',
         lastModified: new Date(2021, 2, 10),
+        visible: false,
         serialisedDiagram: fsm.serialise()
       },
     ];
@@ -47,6 +57,30 @@ export class DiagramManagementModalComponent {
       this.diagramContainer.set(deserialiseDiagram(this.dbEntries[this.selectedIndex].serialisedDiagram));
     }
   }
+
+  triggerDeleteModal() {
+    this.modalService.open(this.deleteModal);
+  }
+
+  delete() {
+    // Todo: Hook this up to the back-end
+    this.dbEntries.splice(this.selectedIndex, 1);
+  }
+
+  updateChanges() {
+    // Todo: Hook this up to the back-end
+  }
+
+  triggerEditModal() {
+    this.modalService.open(this.editModal);
+  }
+
+  toggleVisibility() {
+    if (this.selectedIndex !== -1) {
+      this.dbEntries[this.selectedIndex].visible = !this.dbEntries[this.selectedIndex].visible;
+    }
+    // Todo: Call back-end
+  }
 }
 
 // Todo: Match this up with the actual DB structure once it is known
@@ -54,5 +88,6 @@ export type DatabaseDiagramEntry = {
   id: number,
   title: string,
   lastModified: Date,
+  visible: boolean,
   serialisedDiagram: SerialisedDiagram
 }
