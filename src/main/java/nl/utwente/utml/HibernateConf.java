3fc61a0aaa3426237c2e0e37501fa44611b99331
Andrew Heath <a.j.heath@student.utwente.nl>
2021-03-24 16:28:36 +0100

    Fix .env imports

--- src/main/java/nl/utwente/utml/HibernateConf.java
+++ src/main/java/nl/utwente/utml/HibernateConf.java
@@ -1,8 +1,11 @@
 package nl.utwente.utml;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -17,9 +20,13 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class HibernateConf {
 
-    public static final String JDBC_URL = "jdbc:postgresql://localhost:5433/utml";
-    public static final String JDBC_USERNAME = "admin";
-    public static final String JDBC_PASSWORD = "admin";
+
+    @Value( "${spring.datasource.url}" )
+    public String JDBC_URL ;
+    @Value( "${spring.datasource.username}" )
+    public String JDBC_USERNAME;
+    @Value( "${spring.datasource.password}")
+    public String JDBC_PASSWORD;
 
     @Bean(name="entityManagerFactory")
     public LocalSessionFactoryBean sessionFactory() {
