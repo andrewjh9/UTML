c4f4d475519e459133fb7c516105907bc4eae379
Andrew Heath <a.j.heath@student.utwente.nl>
2021-03-25 09:44:41 +0100

    Revert "remove mvn plugins"
    This reverts commit be14b8dbf81f5aab5a2909f426aea5c998b10605.

--- src/main/java/nl/utwente/utml/UtmlApplication.java
+++ src/main/java/nl/utwente/utml/UtmlApplication.java
@@ -3,9 +3,7 @@ package nl.utwente.utml;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
@@ -15,12 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-public class UtmlApplication extends SpringBootServletInitializer {
+public class UtmlApplication  {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(UtmlApplication .class);
-	}
+//	@RequestMapping("/diagram/{id}")
+//	public String diagrams() {
+//		return "index.html";
+//	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(UtmlApplication.class, args);
 	}
