24eab4a8e3b3e4843c2a33f9dcf8ef3936a4cb8e
= <s2190532@utwente.nl>
2021-04-05 14:26:34 +0200

    Change diagram preview slightly

--- client/src/app/diagram-preview/diagram-preview.component.ts
+++ client/src/app/diagram-preview/diagram-preview.component.ts
@@ -2,10 +2,17 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Diagram} from "../../model/diagram";
 
 @Component({
-  selector: '[diagram-preview]',
+  selector: 'diagram-preview',
   templateUrl: './diagram-preview.component.html',
   styleUrls: ['./diagram-preview.component.scss']
 })
 export class DiagramPreviewComponent {
   @Input() diagram!: Diagram;
+  @Input() width!: number;
+  @Input() height!: number;
+
+  get viewBox() {
+    let dimensions =  this.diagram.getDimensions();
+    return `${dimensions.leftX} ${dimensions.topY} ${dimensions.width} ${dimensions.height};`
+  }
 }
