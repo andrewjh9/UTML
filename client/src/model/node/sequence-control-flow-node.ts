0a0c8289aab344b46b4701eddc00be7cfb907c27
= <s2190532@utwente.nl>
2021-04-06 13:46:02 +0200

    Fix serialisation for SequenceControlFlowNode

--- client/src/model/node/sequence-control-flow-node.ts
+++ client/src/model/node/sequence-control-flow-node.ts
@@ -1,6 +1,7 @@
 import {RectangleNode} from "./rectangle-node";
 import {Position} from "../position";
 import {Node} from "./node";
+import {SerialisedSequenceControlFlowNode} from "../../serialisation/serialised-data-structures/serialised-node";
 
 export class SequenceControlFlowNode extends RectangleNode {
   /** The text that will be displayed in the top-left of the controlFlowNode. */
@@ -22,5 +23,18 @@ export class SequenceControlFlowNode extends RectangleNode {
 
   public getDeepCopy(): Node {
     return new SequenceControlFlowNode(this.width, this.height, this.position.getDeepCopy(), this.name);
+  }
+
+  public serialise(): SerialisedSequenceControlFlowNode {
+    return {
+      width: this.width,
+      height: this.height,
+      position: this.position.serialise(),
+      text: this.text,
+      name: this.name,
+      type: this.getNodeTypeName(),
+      hasDoubleBorder: false,
+      styleObject: Node.copyStyleObject(this.styleObject)
+    }
   }
 }
