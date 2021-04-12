f404e3f9ffb9e16055ad5b3452017481105b5451
Andrew Heath <a.j.heath@student.utwente.nl>
2021-04-12 11:07:31 +0200

    Prevent students from logging in

--- src/main/java/nl/utwente/utml/WebSecurityConfig.java
+++ src/main/java/nl/utwente/utml/WebSecurityConfig.java
@@ -1,12 +1,31 @@
 package nl.utwente.utml;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +37,8 @@ import java.util.Set;
  *
  */
 public class  WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] STUDENT_EMAIL_WHITELIST = new String[]{"a.j.heath@student.utwente.nl","a.j.heath@student.utwente.net", "m.t.voorberg@student.utwente.nl", "p.m.frolov@student.utwente.nl"};
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,6 +61,7 @@ public class  WebSecurityConfig extends WebSecurityConfigurerAdapter {
         /**
          * Setting which routes require authentication
          */
+
         http.authorizeRequests()
                 // allow antonymous access to the root page
                 .antMatchers("/").permitAll()
@@ -53,7 +75,25 @@ public class  WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // enable OAuth2/OIDC
                 .and().oauth2Client()
-                .and().oauth2Login();
+                .and().oauth2Login().userInfoEndpoint()
+                    .oidcUserService(this.oidcUserService());
+    }
+
+    /**
+     * Preventing students from authenticating
+     */
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        final OidcUserService delegate = new OidcUserService();
+        return (userRequest) -> {
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+            String emailDomain = oidcUser.getEmail().substring(oidcUser.getEmail().indexOf("@") + 1);
+            if (emailDomain.equals("utwente.nl") || Arrays.asList(STUDENT_EMAIL_WHITELIST).contains(oidcUser.getEmail())) {
+                return oidcUser;
+            } else{
+                throw new OAuth2AuthenticationException(new OAuth2Error("unauthorized_client", "Students cannot login", ""));
+            }
+        };
     }
 
 }
