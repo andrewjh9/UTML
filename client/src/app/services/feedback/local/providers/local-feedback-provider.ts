c6a07dbd4ceccf4b94bfe5fbea6b486a73754ad7
Marten Voorberg <martenvoorberg@gmail.com>
2021-08-31 13:11:42 +0200

    Remove empty files

--- client/src/app/services/feedback/local/providers/local-feedback-provider.ts
+++ client/src/app/services/feedback/local/providers/local-feedback-provider.ts
@@ -4,9 +4,6 @@ import {Feedback} from '../feedback';
 /**
  * An abstract class acting as an interface for LocalFeedbackProviders.
  *
- * This is an abstract class and not an interface as you might see in Java
- * because TypeScript does not natively support such interfaces.
- *
  * Any localFeedbackProvider should extend this class.
  *
  * Every LocalFeedBackProvider should be accompanied by a LocalFeedbackProviderFactory
